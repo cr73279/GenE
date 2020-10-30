7a74895b012617ddcafb9e97ed6494e30c7dfd22
chris <christopher.richardson8491@gmail.com>
2020-10-29 21:16:02 -0400

    Initial Commit

---
+++ GenE/GenEApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenEApp.swift
+//  GenE
+//
+//  Created by chris richardson on 10/29/20.
+//
+
+import SwiftUI
+
+@main
+struct GenEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
